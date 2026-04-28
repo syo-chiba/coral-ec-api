5c39731914fea295372e0dc2c72bb0582b8916b7
shochiba2 <blacklagoon630803@gmail.com>
2026-04-28 23:28:34 +0900

    Initial commit

---
+++ src/main/java/com/example/coral_ec/CoralEcApplication.java
@@ -0,0 +1,13 @@
+package com.example.coral_ec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoralEcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoralEcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/coral_ec/CoralEcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coral_ec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoralEcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
