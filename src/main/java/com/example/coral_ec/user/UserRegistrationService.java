359e18b3a7b142e48e74e9d40b95d183a557ca58
shochiba2 <blacklagoon630803@gmail.com>
2026-04-29 14:47:55 +0900

    step3の実装

--- src/main/java/com/example/coral_ec/user/UserRegistrationService.java
+++ src/main/java/com/example/coral_ec/user/UserRegistrationService.java
@@ -8,7 +8,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-
+@Service
 public class UserRegistrationService {
 	
 	private final UserRepository userRepository;
